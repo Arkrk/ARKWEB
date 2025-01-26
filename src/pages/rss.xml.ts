c7b8b4565995ce83d4d4652c8126d3bb351d114d
arksg <yuka.towa.1306@gmail.com>
2025-01-26 14:55:02 +0900

    update: RSSフィードに xmlns の定義を追加

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -9,11 +9,21 @@ export async function GET() {
     title: database.Title,
     description: database.Description,
     site: import.meta.env.SITE,
+    xmlns: {
+      content: 'http://purl.org/rss/1.0/modules/content/',
+      wfw: 'http://wellformedweb.org/CommentAPI/',
+      atom: 'http://www.w3.org/2005/Atom',
+      dc: 'http://purl.org/dc/elements/1.1/',
+      sy: 'http://purl.org/rss/1.0/modules/syndication/',
+      slash: 'http://purl.org/rss/1.0/modules/slash/',
+      media: 'http://search.yahoo.com/mrss/',
+    },
     items: posts.map((post) => ({
       link: new URL(getPostLink(post.Slug), import.meta.env.SITE).toString(),
       title: post.Title,
       description: post.Excerpt,
       pubDate: new Date(post.Date),
     })),
+    customData: `<language>ja</language>`,
   })
 }
